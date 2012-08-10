1f3592bc55d1a6c7e0c04f192ebdbfdee8d348a8
breach <breach@breach-comp.(none)>
2012-08-10 14:46:51 +0200

    Fixade focus från editText

--- Vttrfkgit64/gen/com/vsttrfk/git64/R.java
+++ Vttrfkgit64/gen/com/vsttrfk/git64/R.java
@@ -34,17 +34,16 @@ public final class R {
         public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int Läs=0x7f050006;
-        public static final int Skriv=0x7f050007;
-        public static final int StepOneFil=0x7f050002;
-        public static final int StepOneNfc=0x7f050003;
-        public static final int StepTwoFil=0x7f050004;
-        public static final int StepTwoNfc=0x7f050005;
-        public static final int anonExp=0x7f05000a;
-        public static final int app_name=0x7f050001;
-        public static final int fileName=0x7f050009;
-        public static final int hello=0x7f050000;
-        public static final int statusBox=0x7f050008;
+        public static final int Läs=0x7f050005;
+        public static final int Skriv=0x7f050006;
+        public static final int StepOneFil=0x7f050001;
+        public static final int StepOneNfc=0x7f050002;
+        public static final int StepTwoFil=0x7f050003;
+        public static final int StepTwoNfc=0x7f050004;
+        public static final int anonExp=0x7f050009;
+        public static final int app_name=0x7f050000;
+        public static final int fileName=0x7f050008;
+        public static final int statusBox=0x7f050007;
     }
     public static final class xml {
         public static final int nfc_techs=0x7f040000;
