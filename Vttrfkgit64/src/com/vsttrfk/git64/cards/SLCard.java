87112f9cc6f8047a192ed3b657a1c9f4222a19fb
Johannes Wikner <johannes.wikner@gmail.com>
2013-06-25 05:10:41 +0200

    SLCard class

---
+++ Vttrfkgit64/src/com/vsttrfk/git64/cards/SLCard.java
@@ -0,0 +1,36 @@
+package com.vsttrfk.git64.cards;
+
+import java.io.IOException;
+
+import com.vsttrfk.git64.auth.IRKFAuthable;
+
+public class SLCard extends AbstractRKFCard {
+
+	private final String provider = "SL";
+	
+	public SLCard(byte[][] data) throws IOException {
+		super(data);
+	}
+
+	public byte[][] getKeysA() {
+		return IRKFAuthable.KEYS_A_SL;
+	}
+
+	public byte[][] getKeysB() {
+		return IRKFAuthable.KEYS_B_SL;
+	}
+
+	public boolean anonymousExploit() {
+		return false; // not implemented
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	@Override
+	protected int getBalanceBlock() {
+		return 0; // not implemented
+	}
+
+}
